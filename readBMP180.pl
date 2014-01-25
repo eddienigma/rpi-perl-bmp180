d80e4b61beef86d7c85fd8d4b15e1403424ab3b1
EddieNigma <jaseymour@gmail.com>
2014-01-25 10:29:08 -0600

    Defined constant values for registers and variables for holding eeprom calibration data

---
+++ readBMP180.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/perl
+
+# Copyright 2014 by Jason Seymour
+# Rev 1.0
+
+# This is a Perl script to read temperature and barometric
+# pressure from an Adafruit BMP180 breakout board.
+
+use Device::SMBus;
+
+
+# Instantiate an SMBus object referring to the correct I2C bus
+# and the device address of the sensor. The Adafruit BMP180
+# sensor always has an address of 0x77. The rev2 RPi puts the
+# primary I2C bus on i2c-1. The rev1 uses i2c-0.
+
+my $bmp180 = Device::SMBus->new(
+  I2CBusDevicePath => '/dev/i2c-1',
+  I2CDeviceAddress => 0x77
+);
+
+# Define a standard list of operating modes for the sensor.
+# These control the number of samples per second that the 
+# sensor takes internally.
+
+use constant BMP180_ULTRALOWPOWER	=> 0;
+use constant BMP180_STANDARD		=> 1;
+use constant BMP180_HIRES		=> 2;
+use constant BMP180_ULTRAHIRES		=> 3;
+
+# Define the sensor registers. Many of these store calibration data
+# which is used to calculate temperature compensated pressure readings.
+
+use constant BMP180_CAL_AC1		=> 0xAA;	# Calibration data (16 bit)
+use constant BMP180_CAL_AC2		=> 0xAC;	# Calibration data (16 bit) 
+use constant BMP180_CAL_AC3		=> 0xAE;	# Calibration data (16 bit)
+use constant BMP180_CAL_AC4		=> 0xB0;	# Calibration data (16 bit)
+use constant BMP180_CAL_AC5		=> 0xB2;	# Calibration data (16 bit)
+use constant BMP180_CAL_AC6		=> 0xB4;	# Calibration data (16 bit)
+use constant BMP180_CAL_B1		=> 0xB6;	# Calibration data (16 bit)
+use constant BMP180_CAL_B2		=> 0xB8;	# Calibration data (16 bit)
+use constant BMP180_CAL_MB		=> 0xBA;	# Calibration data (16 bit)
+use constant BMP180_CAL_MC		=> 0xBC;	# Calibration data (16 bit)
+use constant BMP180_CAL_MD		=> 0xBE;	# Calibration data (16 bit)
+use constant BMP180_CONTROL		=> 0xF4;
+use constant BMP180_TEMPDATA		=> 0xF6;
+use constant BMP180_PRESSUREDATA	=> 0xF6;
+use constant BMP180_READTEMPCMD		=> 0x2E;
+use constant BMP180_READPRESSURECMD	=> 0x34;
+
+# Define a list of variables to store the calibration data into.
+
+my cal_AC1	= 0;
+my cal_AC2	= 0;
+my cal_AC3	= 0;
+my cal_AC4	= 0;
+my cal_AC5	= 0;
+my cal_AC6	= 0;
+my cal_B1	= 0;
+my cal_B2	= 0;
+my cal_MB	= 0;
+my cal_MC	= 0;
+my cal_MD	= 0;
+
+# The Device::SMBus module provides methods for reading 8 and 16 bit values
+# from the sensor, but these methods don't differentiate between signed and
+# unsigned values. We need to create our own functions to read signed values
+# and handle them correctly.
